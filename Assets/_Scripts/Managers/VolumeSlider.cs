802bd3c531ae0e5d0ba90e0852bb355cf0cbdc72
Matthew Yuen <matthew.yuen@Viewpoint.com>
2022-04-29 16:42:46 -0700

    Add Audio Manager

---
+++ Assets/_Scripts/Managers/VolumeSlider.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+ 
+
+namespace Hackathon
+{
+    public class VolumeSlider : MonoBehaviour
+    {
+        [SerializeField] private Slider _slider;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _slider.value = AudioListener.volume;
+            _slider.onValueChanged.AddListener(val => AudioManager.Instance.ChangeMasterVolume(val));    
+        } 
+    }
+}
