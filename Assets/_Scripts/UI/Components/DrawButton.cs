c2521db04cad38a7181fb341034a4ef4073ccdfc
friesen04 <40907762+friesen04@users.noreply.github.com>
2022-05-20 11:03:39 -0700

    Update Color/Width with server

--- Assets/_Scripts/UI/Components/DrawButton.cs
+++ Assets/_Scripts/UI/Components/DrawButton.cs
@@ -1,25 +1,32 @@
 using UnityEngine;
 using UnityEngine.UI;
+using Mirror;
+using System.Linq;
 
 namespace Hackathon
 {
-    public class DrawButton : MonoBehaviour
+    public class DrawButton : NetworkBehaviour
     {
-        //[SerializeField] private NetworkDrawManager _drawManager;
+        private PlayerController _playerController;
         [SerializeField] private Button _button;
+
+        public void Start()
+        {
+            _playerController = (PlayerController)GameObject.FindObjectsOfType(typeof(PlayerController)).FirstOrDefault(o => ((PlayerController)o).isLocalPlayer);
+        }
 
         public void OnButtonPress()
         {
             if (_button.tag == "Color Button")
             {
                 var buttonColor = _button.GetComponent<Image>().color;
-                //_drawManager._lineColor = buttonColor;
+                _playerController.CmdUpdateColor(buttonColor);
             }
 
             if (_button.tag == "Size Button")
             {
-                var buttonWidth = GetComponent<RectTransform>().sizeDelta.x;
-                //_drawManager._lineWidth = buttonWidth;
+                var buttonWidth = _button.GetComponent<RectTransform>().sizeDelta.x;
+                _playerController.CmdUpdateWidth(buttonWidth);
             }
         }
     }
